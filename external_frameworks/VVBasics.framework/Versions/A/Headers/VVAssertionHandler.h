21ac4e7a23c99f233d58c57facd57042e114b426
ray cutler <ray@vidvox.net>
2017-12-08 07:16:16 -0500

    adding external frameworks

---
+++ external_frameworks/VVBasics.framework/Versions/A/Headers/VVAssertionHandler.h
@@ -0,0 +1,34 @@
+#import <TargetConditionals.h>
+#import "VVBasicMacros.h"
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#else
+#import <Cocoa/Cocoa.h>
+#endif
+
+
+
+
+#define USE_CUSTOM_ASSERTION_HANDLER																			\
+{																												\
+	NSThread				*__currentThread = [NSThread currentThread];										\
+	NSDictionary			*__threadDict = (__currentThread==nil) ? nil : [__currentThread threadDictionary];	\
+	if (__threadDict != nil)	{																				\
+		VVAssertionHandler		*__newAH = [[VVAssertionHandler alloc] init];									\
+		if (__newAH != nil)	{																					\
+			[__threadDict setValue:__newAH forKey:@"NSAssertionHandler"];										\
+#if USING_MRC																									\
+			[__newAH release];																					\
+#endif																											\
+			__newAH = nil;																						\
+		}																										\
+	}																											\
+}
+
+
+
+@interface VVAssertionHandler : NSAssertionHandler {
+
+}
+
+@end
